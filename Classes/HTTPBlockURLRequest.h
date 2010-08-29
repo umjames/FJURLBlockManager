984b13437d1a0ddb376197b81eff5d6e5497f6aa
Michael James <umjames29@gmail.com>
2010-08-29 17:24:54 -0400

    Added HTTPBlockURLRequest class
    HTTPBlockURLRequest is an FJBlockURLRequest subclass that allows client
    code to access the NSURLResponse (if it's an NSHTTPURLResponse object).
    It also calls the failure block if the HTTP response status code is >=
    400.

---
+++ Classes/HTTPBlockURLRequest.h
@@ -0,0 +1,20 @@
+//
+//  HTTPBlockURLRequest.h
+//  FJBlockURLManager
+//
+//  Created by umjames on 8/29/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "FJBlockURLRequest.h"
+
+extern NSString* const HTTPBlockURLErrorDomain;
+
+@interface HTTPBlockURLRequest : FJBlockURLRequest
+{
+}
+
+@property (nonatomic, readonly, retain) NSHTTPURLResponse*	HTTPResponse;
+
+@end
---
+++ Classes/HTTPBlockURLRequest.m
@@ -0,0 +1,67 @@
+//
+//  HTTPBlockURLRequest.m
+//  FJBlockURLManager
+//
+//  Created by umjames on 8/29/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "HTTPBlockURLRequest.h"
+
+NSString* const HTTPBlockURLErrorDomain = @"HTTPBlockURLErrorDomain";
+
+@interface HTTPBlockURLRequest ()
+
+@property (nonatomic, readwrite, retain) NSHTTPURLResponse*	HTTPResponse;
+
+- (BOOL)_responseIsSuccessful;
+
+@end
+
+@implementation HTTPBlockURLRequest
+
+@synthesize HTTPResponse;
+
+- (void)dealloc
+{
+	[HTTPResponse release];
+	HTTPResponse = nil;
+	
+	[super dealloc];
+}
+
+- (void)connection: (NSURLConnection*)connection didReceiveResponse: (NSURLResponse*)response
+{    
+    [super connection: connection didReceiveResponse: response];
+    
+	if ([response isKindOfClass: [NSHTTPURLResponse class]])
+	{
+		self.HTTPResponse = (NSHTTPURLResponse*)response;
+	}
+}
+
+- (void)connectionDidFinishLoading: (NSURLConnection*)connection
+{    
+    if ([self _responseIsSuccessful])
+	{
+		[super connectionDidFinishLoading: connection];
+	}
+	else
+	{
+		NSError*	HTTPError = [NSError errorWithDomain: HTTPBlockURLErrorDomain code: [self.HTTPResponse statusCode] userInfo: nil];
+		
+		[self connection: connection didFailWithError: HTTPError];
+	}
+}
+
+- (BOOL)_responseIsSuccessful
+{
+	if (nil == self.HTTPResponse)
+	{
+		return NO;
+	}
+	
+	return ([self.HTTPResponse statusCode] < 400);
+}
+
+@end
